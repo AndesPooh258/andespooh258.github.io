266e75f6ad85f05a6d0819f22e7bd7228e319260
AndesPooh258 <andespooh258@gmail.com>
2022-08-31 04:57:26 +0800

    Simplified the code

--- js/script.js
+++ js/script.js
@@ -4,84 +4,73 @@ window.onscroll = function(){
 	let progress = (window.scrollY / (document.body.clientHeight - window.innerHeight)) * 100;
 	let progress_bar = document.querySelector("#main-progress-bar");
 	let to_top = document.querySelector("#main-to-top");
+	let to_top_threshold = 10;
 	// update progress bar
-	progress_bar.style.width = progress.toString() + "%";
-	// update to-top button with animation
-	if (progress > 10){
-		to_top.style.animation = "fade-in 0.5s";
-		setTimeout(() => to_top.style.display = "block", 500);
-	} else {
-		to_top.style.animation = "fade-out 0.5s";
-		setTimeout(() => to_top.style.display = "none", 500);
+	if (progress_bar != null){
+		progress_bar.style.width = progress.toString() + "%";
 	}
-	
+	// update to-top button with animation
+	if (to_top != null) {
+		if (progress > to_top_threshold && to_top.classList.contains("hide")){
+			to_top.className = "to-top-btn";
+		} else if (progress <= to_top_threshold && !to_top.classList.contains("hide")){
+			to_top.style.animation = "fade-out 0.5s";
+			setTimeout(() => to_top.removeAttribute('style') || (to_top.className = "to-top-btn hide"), 500);
+		}
+	}
 }
 
 function toggleSpecialBar(){
-	let special_bar_hide = document.querySelector("#special-bar.hide");
-	let special_bar = document.querySelector("#special-bar.navbar");
+	let special_bar = document.querySelector("#nav-special-bar");
 	// update special bar
-	if(special_bar_hide != null){
-		special_bar_hide.className = "navbar navbar-expand-lg justify-content-center my-sm-3";
-	}
-	if(special_bar != null){
-		special_bar.className = "hide";
+	if (special_bar != null){
+		special_bar.className = special_bar.classList.contains("hide") ? "navbar navbar-expand-lg justify-content-center my-sm-3" : "hide";
 	}
 }
 
 function align(){
-	let left_list = document.querySelectorAll(".section-heading.text-start");
-	let center_list = document.querySelectorAll(".section-heading.text-center");
-	let right_list = document.querySelectorAll(".section-heading.text-end");
+	let section_headings = document.querySelectorAll(".section-heading");
+	let heading_className = "section-heading ";
 	// update section headings
-	for(let i = 0; i < left_list.length; i++){
-		left_list[i].className = "section-heading text-center";
-	}
-	for(let i = 0; i < center_list.length; i++){
-		center_list[i].className = "section-heading text-end";
-	}
-	for(let i = 0; i < right_list.length; i++){
-		right_list[i].className = "section-heading text-start";
+	if (section_headings != null && section_headings.length > 0){
+		heading_className += section_headings[0].classList.contains("text-start") ? "text-center" : 
+							 section_headings[0].classList.contains("text-center") ? "text-end" : "text-start";
+		for(let i = 0; i < section_headings.length; i++){
+			section_headings[i].className = heading_className;
+		}
 	}
 }
 
 function toggleDark(){
-	let table = document.querySelector(".table");
-	let light_list = document.querySelectorAll(".navbar.bg-light");
-	let dark_list = document.querySelectorAll(".navbar.bg-dark");
 	let theme = document.querySelector("#theme-css");
-	let dark_btn = document.querySelector("#dark-btn");
-	let is_light = light_list.length > 0;
-	// update table
-	if (table != null){
-		table.classList.toggle("table-dark");
-	}
+	let navbar = document.querySelector("#nav-navbar");
+	let dark_btn = document.querySelector("#nav-dark-btn");
+	let tables = document.querySelectorAll(".table");
+	let is_light = navbar.classList.contains("bg-light");
+	let nav_className = "navbar navbar-expand-md justify-content-center my-sm-3 ";
 	// update css
 	if(theme != null){
 		theme.href = is_light ? "./css/style-dark.css" : "./css/style-light.css";
+	}
+	// update navbar
+	if (navbar != null){
+		navbar.className = nav_className + (is_light ? "bg-dark" : "bg-light");
 	}
 	// update dark mode button
 	if (dark_btn != null){
 		dark_btn.textContent = is_light ? "Light" : "Dark";
 	}
-	// update navbar
-	for(let i = 0; i < light_list.length; i++){
-		light_list[i].className = "navbar navbar-expand-md bg-dark justify-content-center my-sm-3";
-	}
-	for(let i = 0; i < dark_list.length; i++){
-		dark_list[i].className = "navbar navbar-expand-md bg-light justify-content-center my-sm-3";
+	// update tables
+	for(let i = 0; tables != null && i < tables.length; i++){
+		tables[i].classList.toggle("table-dark");
 	}
 }
 
 function toggleScrollBar(){
-	let scroll_bar_hide = document.querySelector("#scroll-bar.hide");
-	let scroll_bar = document.querySelector("#scroll-bar.progress");
+	let scroll_bar = document.querySelector("#main-scroll-bar");
 	// update scroll bar
-	if(scroll_bar_hide != null){
-		scroll_bar_hide.className = "progress sticky-top";
-	}
 	if(scroll_bar != null){
-		scroll_bar.className = "hide";
+		scroll_bar.className = scroll_bar.classList.contains("hide") ? "progress sticky-top" : "hide";
 	}
 }
 
@@ -98,9 +87,9 @@ function popupInit(){
 	let popup_img = document.querySelector("#main-popup-img");
 	let popup_caption = document.querySelector("#main-popup-caption");
 	let popup_close = document.querySelector("#main-popup-close");
-	let major_gpa = document.querySelector("#major-gpa");
-	let ptcg_deck = document.querySelector("#ptcg-deck");
-	// set onclick event on close button
+	let major_gpa = document.querySelector("#popup-major-gpa");
+	let ptcg_deck = document.querySelector("#popup-ptcg-deck");
+	// set onClick event on close button
 	popup_close.onclick = function(){
 		popup_box.classList.toggle("hide");
 	}
@@ -117,14 +106,14 @@ function popupInit(){
 function changeImage(){
 	let default_img = 0;
 	let num_files = 12;
-	let pic = document.querySelector("#about-pic");
-	// update image
-	if(pic != null){
-		let img = parseInt(pic.alt, 10);
+	let about_pic = document.querySelector("#about-pic");
+	// update about image
+	if(about_pic != null){
+		let img = parseInt(about_pic.alt, 10);
 		if (Number.isNaN(img)){
 			img = default_img;
 		} else img = ++img % num_files;
-		pic.alt = img;
-		pic.src = "./img/about/image_" + img + ".jpg";
+		about_pic.alt = img;
+		about_pic.src = "./img/about/image_" + img + ".jpg";
 	}
 }
